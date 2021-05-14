7ac108821ac035e2e70a513e55f42cb0b9138f3c
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-05-15 00:19:27 +0200

    add a function getting user name value and passe it to the header game

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 import './sass/build.scss';
+
+const loginPanelBackground = document.querySelector(
+    '.login-panel-overlay-bg--js',
+);
+const userNameValue = document.querySelector('.login-panel__input--js');
+const userName = document.querySelector('.header__user-name--js');
+const btnLogin = document.querySelector('.login-panel__btn--js');
+
+const game = {
+    userName: userNameValue,
+};
+
+const getUserName = () => {
+    loginPanelBackground.style.display = 'none';
+    if (!game.userName.value) {
+        userName.textContent = `Welcome, unknown`;
+    } else {
+        userName.textContent = `Welcome, ${game.userName.value}`;
+    }
+};
+
+btnLogin.addEventListener('click', getUserName);
