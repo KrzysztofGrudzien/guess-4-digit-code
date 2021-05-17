9ae7d105af890e39ad1ff9d3c86ff7bc8b723cdb
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-05-17 18:53:56 +0200

    add a functionality for totalScore and highscore

--- src/app.js
+++ src/app.js
@@ -24,10 +24,17 @@ const padlockFirst = document.querySelector('.card__padlock-first--js');
 const padlockSecond = document.querySelector('.card__padlock-second--js');
 const padlockThird = document.querySelector('.card__padlock-third--js');
 const padlockFourth = document.querySelector('.card__padlock-fourth--js');
+const mainTitle = document.querySelector('.main-title--js');
+const secondTitle = document.querySelector('.main-title--description--js');
+const totalScore = document.querySelector('.score__total--js');
+const highScore = document.querySelector('.score__highscore--js');
 
 const game = {
     userName: userNameValue,
     randomNumbers: [],
+    checkNumbers: [],
+    totalScore: 20,
+    highScore: 0,
 };
 
 const getUserName = () => {
@@ -42,7 +49,7 @@ const getUserName = () => {
 btnLogin.addEventListener('click', getUserName);
 
 userNameValue.addEventListener('keyup', e => {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 || e.keyCode === 27) {
         getUserName();
     }
 });
@@ -53,7 +60,7 @@ for (let i = 0; i < 4; i++) {
     game.randomNumbers.push(randomNumber());
 }
 
-btnCheckFirstDigit.addEventListener('click', () => {
+btnCheckFirstDigit.addEventListener('click', function () {
     if (parseInt(digitFirstValue.value) === game.randomNumbers[0]) {
         digitFirst.textContent = parseInt(digitFirstValue.value);
         digitFirst.style.backgroundColor = '#57f58d';
@@ -62,18 +69,37 @@ btnCheckFirstDigit.addEventListener('click', () => {
         btnCheckFirstDigit.style.backgroundColor = '#d7d7d7';
         digitFirstValue.setAttribute('disabled', true);
         btnCheckFirstDigit.setAttribute('disabled', true);
+        game.checkNumbers[0] = parseInt(digitFirstValue.value);
+        const randomNumbersSum = game.randomNumbers.reduce((acc, digit) => acc + digit);
+        const checkNumbersSum = game.checkNumbers.reduce((acc, digit) => acc + digit);
+
+        if (game.totalScore > game.highScore) {
+            game.highScore = game.totalScore;
+            highScore.textContent = game.highScore;
+        }
+        totalScore.textContent = game.totalScore;
+
+        if (randomNumbersSum === checkNumbersSum) {
+            mainTitle.style.fontWeight = '800';
+            mainTitle.textContent = 'YOU WIN 🏆🏆🏆';
+            secondTitle.style.display = 'none';
+        }
     } else if (parseInt(digitFirstValue.value) < game.randomNumbers[0]) {
         digitFirst.textContent = parseInt(digitFirstValue.value);
         digitFirst.style.backgroundColor = '#f37171';
         digitFirstInfo.textContent = 'Too Low 📉';
         padlockFirst.textContent = '🔐';
         btnCheckFirstDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     } else {
         digitFirst.textContent = parseInt(digitFirstValue.value);
         digitFirst.style.backgroundColor = '#f37171';
         digitFirstInfo.textContent = 'Too high 📈';
         padlockFirst.textContent = '🔐';
         btnCheckFirstDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     }
 });
 
@@ -86,18 +112,36 @@ btnCheckSecondDigit.addEventListener('click', () => {
         btnCheckSecondDigit.style.backgroundColor = '#d7d7d7';
         digitSecondValue.setAttribute('disabled', true);
         btnCheckSecondDigit.setAttribute('disabled', true);
+        game.checkNumbers[1] = parseInt(digitSecondValue.value);
+        const randomNumbersSum = game.randomNumbers.reduce((acc, digit) => acc + digit);
+        const checkNumbersSum = game.checkNumbers.reduce((acc, digit) => acc + digit);
+        if (game.totalScore > game.highScore) {
+            game.highScore = game.totalScore;
+            highScore.textContent = game.highScore;
+        }
+        totalScore.textContent = game.totalScore;
+
+        if (randomNumbersSum === checkNumbersSum) {
+            mainTitle.style.fontWeight = '800';
+            mainTitle.textContent = 'YOU WIN 🏆🏆🏆';
+            secondTitle.style.display = 'none';
+        }
     } else if (parseInt(digitSecondValue.value) < game.randomNumbers[1]) {
         digitSecond.textContent = parseInt(digitSecondValue.value);
         digitSecond.style.backgroundColor = '#f37171';
         digitSecondInfo.textContent = 'Too Low 📉';
         padlockSecond.textContent = '🔐';
         btnCheckSecondDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     } else {
         digitSecond.textContent = parseInt(digitSecondValue.value);
         digitSecond.style.backgroundColor = '#f37171';
         digitSecondInfo.textContent = 'Too high 📈';
         padlockSecond.textContent = '🔐';
         btnCheckSecondDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     }
 });
 
@@ -110,18 +154,36 @@ btnCheckThirdDigit.addEventListener('click', () => {
         btnCheckThirdDigit.style.backgroundColor = '#d7d7d7';
         digitThirdValue.setAttribute('disabled', true);
         btnCheckThirdDigit.setAttribute('disabled', true);
+        game.checkNumbers[2] = parseInt(digitThirdValue.value);
+        const randomNumbersSum = game.randomNumbers.reduce((acc, digit) => acc + digit);
+        const checkNumbersSum = game.checkNumbers.reduce((acc, digit) => acc + digit);
+        if (game.totalScore > game.highScore) {
+            game.highScore = game.totalScore;
+            highScore.textContent = game.highScore;
+        }
+        totalScore.textContent = game.totalScore;
+
+        if (randomNumbersSum === checkNumbersSum) {
+            mainTitle.style.fontWeight = '800';
+            mainTitle.textContent = 'YOU WIN 🏆🏆🏆';
+            secondTitle.style.display = 'none';
+        }
     } else if (parseInt(digitThirdValue.value) < game.randomNumbers[2]) {
         digitThird.textContent = parseInt(digitThirdValue.value);
         digitThird.style.backgroundColor = '#f37171';
         digitThirdInfo.textContent = 'Too Low 📉';
         padlockThird.textContent = '🔐';
         btnCheckThirdDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     } else {
         digitThird.textContent = parseInt(digitThirdValue.value);
         digitThird.style.backgroundColor = '#f37171';
         digitThirdInfo.textContent = 'Too high 📈';
         padlockThird.textContent = '🔐';
         btnCheckThirdDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     }
 });
 
@@ -134,19 +196,35 @@ btnCheckFourthDigit.addEventListener('click', () => {
         btnCheckFourthDigit.style.backgroundColor = '#d7d7d7';
         digitFourthValue.setAttribute('disabled', true);
         btnCheckFourthDigit.setAttribute('disabled', true);
+        game.checkNumbers[3] = parseInt(digitFourthValue.value);
+        const randomNumbersSum = game.randomNumbers.reduce((acc, digit) => acc + digit);
+        const checkNumbersSum = game.checkNumbers.reduce((acc, digit) => acc + digit);
+        if (game.totalScore > game.highScore) {
+            game.highScore = game.totalScore;
+            highScore.textContent = game.highScore;
+        }
+        totalScore.textContent = game.totalScore;
+
+        if (randomNumbersSum === checkNumbersSum) {
+            mainTitle.style.fontWeight = '800';
+            mainTitle.textContent = 'YOU WIN 🏆🏆🏆';
+            secondTitle.style.display = 'none';
+        }
     } else if (parseInt(digitFourthValue.value) < game.randomNumbers[3]) {
         digitFourth.textContent = parseInt(digitFourthValue.value);
         digitFourth.style.backgroundColor = '#f37171';
         digitFourthInfo.textContent = 'Too Low 📉';
         padlockFourth.textContent = '🔐';
         btnCheckFourthDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     } else {
         digitFourth.textContent = parseInt(digitFourthValue.value);
         digitFourth.style.backgroundColor = '#f37171';
         digitFourthInfo.textContent = 'Too high 📈';
         padlockFourth.textContent = '🔐';
         btnCheckFourthDigit.style.backgroundColor = '#62bcee';
+        game.totalScore--;
+        totalScore.textContent = game.totalScore;
     }
 });
-
-console.log(game.randomNumbers);
