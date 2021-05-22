d412c3a503b7bf4d78b759b7127004bf7be8dd16
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-05-22 18:25:40 +0200

    create a function when user win and separate for confetti animation

---
+++ src/helpers/confettiAnimation.js
@@ -0,0 +1,43 @@
+import confetti from 'canvas-confetti';
+
+const confettiAnimation = () => {
+    const canvas = document.createElement('canvas');
+    canvas.classList.add('canvas');
+    document.body.appendChild(canvas);
+    const duration = 15 * 300;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 200, zIndex: 0 };
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    let interval = setInterval(function () {
+        let timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        let particleCount = 50 * (timeLeft / duration);
+
+        confetti(
+            Object.assign({}, defaults, {
+                particleCount,
+                origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+            }),
+        );
+        confetti(
+            Object.assign({}, defaults, {
+                particleCount,
+                origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+            }),
+        );
+    }, 150);
+
+    setTimeout(() => {
+        canvas.remove();
+    }, 7000);
+};
+
+export default confettiAnimation;
